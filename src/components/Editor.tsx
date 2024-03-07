a00a0e8336d7694836284f67f5085033a3f68f04
deelaurence <laurie143pelumi@gmail.com>
2024-03-07 10:04:55 +0100

    first commit

---
+++ src/components/Editor.tsx
@@ -0,0 +1,80 @@
+"use client";
+import Image  from "next/image";
+import logo from '../../public/favicon.png'
+import { fabric } from "fabric";
+import React, { useEffect, useState } from "react";
+import { StoreContext } from "@/store";
+import { observer } from "mobx-react";
+import { Resources } from "./Resources";
+import { ElementsPanel } from "./panels/ElementsPanel";
+import { Menu } from "./Menu";
+import { TimeLine } from "./TimeLine";
+import { Store } from "@/store/Store";
+import "@/utils/fabric-utils";
+
+export const EditorWithStore = () => {
+  const [store] = useState(new Store());
+  return (
+    <StoreContext.Provider value={store}>
+      <Editor></Editor>
+    </StoreContext.Provider>
+  );
+}
+
+export const Editor = observer(() => {
+  const store = React.useContext(StoreContext);
+
+  useEffect(() => {
+    const canvas = new fabric.Canvas("canvas", {
+      height: 500,
+      width: 800,
+      backgroundColor: "#ededed",
+    });
+    fabric.Object.prototype.transparentCorners = false;
+    fabric.Object.prototype.cornerColor = "#00a0f5";
+    fabric.Object.prototype.cornerStyle = "circle";
+    fabric.Object.prototype.cornerStrokeColor = "#0063d8";
+    fabric.Object.prototype.cornerSize = 10;
+    // canvas mouse down without target should deselect active object
+    canvas.on("mouse:down", function (e) {
+      if (!e.target) {
+        store.setSelectedElement(null);
+      }
+    });
+
+    store.setCanvas(canvas);
+    fabric.util.requestAnimFrame(function render() {
+      canvas.renderAll();
+      fabric.util.requestAnimFrame(render);
+    });
+  }, []);
+  return (
+    <>
+    <div className="flex flex-col items-center justify-center gap-2 py-3 bg-slate-200">
+      <img className="h-[40px] w-[40px]" src='favicon.png' alt="logo" />
+      <p className="font-semibold">SKYE<span className="text-blue-500">STUDIO</span></p>
+    </div>
+      <div className="grid grid-rows-[500px_1fr_20px] grid-cols-[72px_300px_1fr_250px] h-[100svh]">
+      <div className="tile row-span-2 flex flex-col bg-slate-200">
+        <Menu />
+      </div>
+      <div className="row-span-2 flex flex-col overflow-hidden">
+        <Resources />
+      </div>
+      <div id="grid-canvas-container" className="col-start-3 bg-slate-200 flex justify-center items-center">
+        <canvas id="canvas" className="h-[500px] w-[800px] row" />
+      </div>
+      <div className="col-start-4 row-start-1 overflow-hidden">
+        <ElementsPanel />
+      </div>
+      <div className="col-start-3 row-start-2 col-span-2 relative px-[10px] py-[4px] overflow-hidden">
+        <TimeLine />
+      </div>
+      {/* <div className="col-span-4 text-right px-2 text-[0.5em] bg-black text-white">
+        Crafted By Amit Digga
+      </div> */}
+    </div>
+    </>
+
+  );
+});
