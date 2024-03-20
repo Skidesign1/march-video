6fae7dc4fe443427b8d13e79c836567b767ffbed
deelaurence <laurie143pelumi@gmail.com>
2024-03-20 13:29:33 +0100

    updte config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,24 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   webpack: (config ) => {
+//     config.externals.push({
+//       sharp: "commonjs sharp",
+//       canvas: "commonjs canvas",
+//     });
+//     return config;
+//   },
+// };
+
+// module.exports = nextConfig;
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config ) => {
-    config.externals.push({
-      sharp: "commonjs sharp",
-      canvas: "commonjs canvas",
-    });
+  experimental: {
+    serverComponentsExternalPackages: ['canvas'],
+    esmExternals: "loose",
+  },
+  webpack: (config) => {
+    config.externals = [...config.externals, { canvas: "canvas" }];  // required to make Konva & react-konva work
     return config;
   },
 };
