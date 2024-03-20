cc4c767edd16aedc86bb283ac8ad83a81735039b
deelaurence <laurie143pelumi@gmail.com>
2024-03-20 09:20:54 +0100

    engine updte

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, {isServer}) => {
-    if(!isServer){
-      config.node={
-        //ensure windows is not defined in server bundle
-        global:false,
-      }
-    }
-    if(isServer){
-      config.node={
-        //include __dirname for server bundle
-        __dirname:true,
-        __filename:true
-      }
-    }
+  webpack: (config, ) => {
     config.externals.push({
       sharp: "commonjs sharp",
       canvas: "commonjs canvas",
