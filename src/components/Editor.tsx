ec856d756e53f10dbda880bd9f76d6c2d1f7a32e
deelaurence <laurie143pelumi@gmail.com>
2024-03-20 20:40:23 +0100

    bit-by-bit-three

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -2,6 +2,7 @@
 import Image  from "next/image";
 import logo from '../../public/favicon.png'
 import { fabric } from "fabric";
+import { useParams } from 'next/navigation'
 import React, { useEffect, useState } from "react";
 import { StoreContext } from "@/store";
 import { Store } from "@/store/Store";
@@ -11,6 +12,8 @@ import { ElementsPanel } from "./panels/ElementsPanel";
 import { Menu } from "./Menu";
 import { TimeLine } from "./TimeLine";
 import "@/utils/fabric-utils";
+import axios from "axios";
+
 
 export const EditorWithStore = () => {
   const [store] = useState(new Store());
@@ -23,6 +26,7 @@ export const EditorWithStore = () => {
 
 export const Editor = observer(() => {
   const store = React.useContext(StoreContext);
+  const params = useParams()
   let page = store.page
   let portrait=store.portrait
   const portraitView = ()=>{
@@ -67,13 +71,48 @@ export const Editor = observer(() => {
       canvas.renderAll();
       fabric.util.requestAnimFrame(render);
     });
+      const fetchTemplateInfo = async () => {
+        try {
+          if(typeof window !== 'undefined'){
+          const token = window.sessionStorage.getItem('token');
   
+          // Check if token exists
+          if (!token) {
+            // Handle case where token is not available
+            console.error('Token not found in session.');
+            return;
+          }
+  
+          // Define request headers with Authorization header containing the token
+          const config = {
+            headers: {
+              Authorization: `${token}`,
+            },
+          };
+          // Make a POST request to fetch template info using the provided id
+          const response = await axios.get(`https://skyestudio-backend.onrender.com/templates/${params.templateId}`, config);
+          console.log(response.data)
+          console.log(response.data.data.Category)
+          const jsonData = response.data.data.templateFile
+          canvas.loadFromJSON(jsonData, function() {
+          canvas.renderAll();
+      });
+         }
+        } catch (error) {
+          console.error('Error fetching template info:', error);
+        }
+      };
+      if (params.templateId) {
+        fetchTemplateInfo();
+      }
+      return
+    
   }, [portrait, page]);
   return (
     <>
     <div className="flex  justify-between items-center  gap-2 py-3 bg-slate-200">
-      <img className="h-[40px] " src='Logo.png' alt="logo" />
-      
+      <img className="h-[40px] " src='/Logo.png' alt="logo" />
+      <button className="bg-purple-400 text-white p-4 cursor-pointer" onClick={() => store?.saveVideo()}>Save Video</button>
     </div>
       <div className="grid grid-rows-[500px_1fr_20px] grid-cols-[72px_300px_1fr_250px] ">
       <div className="tile row-span-2 flex flex-col bg-slate-200">
